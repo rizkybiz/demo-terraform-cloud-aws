0ea26afde250e7ac04a3b3f270da208a112f8223
rizkybiz <justin.defrank@hashicorp.com>
2021-10-07 16:28:32 -0400

    made some big changes

--- outputs.tf
+++ outputs.tf
@@ -6,8 +6,3 @@ output "catapp_url" {
 output "catapp_ip" {
   value = "http://${aws_eip.demo-eip.public_ip}"
 }
-
-output "ssh_priv_key" {
-  value     = tls_private_key.demo-tls-key.private_key_pem
-  sensitive = true
-}
